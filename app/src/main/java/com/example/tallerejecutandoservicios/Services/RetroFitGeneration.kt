1239599a68b25516c84387cc1d1d596df36b4f8e
Krhistian Alejandro Gonzalez Duarte <krhistian.gonzalez@vasslatam.com>
2022-07-13 05:03:15 -0500

    Creacion de clase RetroFitGeneration para proceso de token de login y retoro de retrofit

---
+++ app/src/main/java/com/example/tallerejecutandoservicios/Services/RetroFitGeneration.kt
@@ -0,0 +1,43 @@
+package com.example.tallerejecutandoservicios.Services
+
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetroFitGeneration {
+
+    //Me permite des de cualquier parte del codigo invocar la funcion sin instanciar la clase
+    companion object{
+
+        //Retorna el retrofit que permite crear las clases
+        fun getInstance() {
+
+            //Proceso de login
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .writeTimeout(0, TimeUnit.MILLISECONDS)
+                .readTimeout(2, TimeUnit.MINUTES)
+                .connectTimeout(1, TimeUnit.MINUTES).build()
+
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssX")
+                .create()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://dog.ceo/api/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+
+        }
+
+    }
+
+}
